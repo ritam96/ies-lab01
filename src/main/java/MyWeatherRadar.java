dff1a0c07d7f4b264f841588bc34d369b965c206
Rita Cerdeira <rita.cerdeira@betvictor.com>
2020-10-21 21:07:07 +0100

    Added project code.

---
+++ src/main/java/MyWeatherRadar.java
@@ -0,0 +1,45 @@
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.logging.Logger;
+
+public class MyWeatherRadar {
+
+    private static int CITY_ID;
+    /*
+    loggers provide a better alternative to System.out.println
+    https://rules.sonarsource.com/java/tag/bad-practice/RSPEC-106
+     */
+    private static final Logger logger = Logger.getLogger(MyWeatherRadar.class.getName());
+
+    public static void main(String[] args) {
+
+        CITY_ID = Integer.parseInt(args[0]);
+         /*
+        get a retrofit instance, loaded with the GSon lib to convert JSON into objects
+         */
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ipma.pt/open-data/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        IpmaService service = retrofit.create(IpmaService.class);
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID);
+
+        try {
+            Response<IpmaCityForecast> apiResponse = callSync.execute();
+            IpmaCityForecast forecast = apiResponse.body();
+
+            if (forecast != null) {
+                logger.info( "max temp for today: " + forecast.getData().
+                        listIterator().next().getTMax());
+            } else {
+                logger.info( "No results!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
